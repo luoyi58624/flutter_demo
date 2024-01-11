129de1f794282d04d1875f1a17f48c3e6bcb6e45
luoyi <luoyi58624@163.com>
2024-01-11 15:19:30 +0800

    as

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   debugPrint('init');
   debugPrint('hello');
-  runApp(Container(
-    color: const Color(0xFFB71C1C),
-  ));
+  runApp(const _MyApp());
+}
+
+class _MyApp extends StatelessWidget {
+  const _MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('首页'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text('hello'),
+          ),
+        ),
+      ),
+    );
+  }
 }
