0aabc6d6f33895e087da5a450ddf21508d0d332a
luoyi <luoyi58624@163.com>
2024-01-11 17:16:07 +0800

    1.0.9

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter/my_flutter.dart';
+import 'package:my_flutter_local_notify/my_flutter_local_notify.dart';
+
+late final LocalNotifyChannel imNotifyChannel;
 
 void main() async {
-  LoggerUtil.i('xxxx');
   await initMyFlutter();
-  runApp(const _MyApp());
+  CommonUtil.delayed(1000, () async {
+    await LocalNotifyUtil.init(onClick: (model) {
+      if (model.channelId == imNotifyChannel.channelId) {
+        RouterUtil.to(const ChildPage(title: '通知跳转的页面'));
+      } else {
+        LoggerUtil.i(model);
+        ToastUtil.showToast(model.title);
+      }
+    });
+    imNotifyChannel = await LocalNotifyUtil.createNotifyChannel('聊天通知');
+  });
+  runApp(const MainApp());
 }
 
-class _MyApp extends StatelessWidget {
-  const _MyApp();
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MyApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('首页'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              ToastUtil.showToast('哈喽');
-            },
-            child: const Text('hello'),
-          ),
+    return const MyApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('首页'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                LocalNotifyUtil.send('global ${LocalNotifyUtil.currentMessageId}', '全局消息内容');
+              },
+              child: const Text('发送系统通知'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                LocalNotifyUtil.send(
+                  'IM ${LocalNotifyUtil.currentMessageId}',
+                  '聊天消息内容',
+                  notifyChannel: imNotifyChannel,
+                );
+              },
+              child: const Text('发送IM通知'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                LocalNotifyUtil.cancelAll();
+              },
+              child: const Text('清空通知'),
+            ),
+          ],
         ),
       ),
     );
